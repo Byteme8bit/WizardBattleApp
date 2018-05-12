71df0986d19f88a436eefbab7cc2ce87501886cb
Byteme8bit <lynchpcconsulting@gmail.com>
2018-05-12 00:35:16 -0700

    Third commit Updating program.py Updating actors.py

--- program.py
+++ program.py
@@ -19,14 +19,14 @@ def print_header():
 def game_loop():
 
     creatures = [
-        Creature(),
-        Creature(),
-        Creature(),
-        Creature(),
-        Creature(),
+        Creature('Toad', 1),
+        Creature('Tiger', 12),
+        Creature('Bat', 3),
+        Creature('Dragon', 50),
+        Creature('Evil Wizard', 1000),
     ]
 
-    hero = Wizard()
+    hero = Wizard('Gandolf', 75)
 
 
 
